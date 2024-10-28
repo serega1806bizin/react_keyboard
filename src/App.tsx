b3bd0abbeaef4811b10e53848588ae379d482314
Serhii Bizin <seregabizin@icloud.com>
2024-10-28 21:22:28 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type AppState = {
+  pressedKey: string | null;
+};
+
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      pressedKey: null,
+    };
+  }
+
+  // Add the event listener on component mount
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  // Remove the event listener when the component is unmounted
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  // Update state when a key is pressed
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div>
+        {pressedKey
+          ? `The last pressed key is [${pressedKey}]`
+          : 'Nothing was pressed yet'}
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import App from './App';
 
 import './index.scss';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(<App />);
